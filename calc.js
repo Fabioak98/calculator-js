8e7b9c5ea8a8c746fec45cd0dc50a09fa893e998
Fabio A. Kanashiro <90659464+Fabioak98@users.noreply.github.com>
2022-08-28 22:35:57 -0300

    Add files via upload

---
+++ calc.js
@@ -0,0 +1,98 @@
+const display = document.querySelector('.display');
+const keyNum = document.querySelectorAll('[key-num]');
+const keyOp = document.querySelectorAll('[key-op]');
+const keyEq = document.querySelector('[key-equal]');
+
+let pastNum = null;
+let currOp = null;
+let lastKey = null;
+const opSimbol = ['+','-','/','X']
+
+
+function onLoad(){
+    display.textContent = '0'
+}
+
+function calculate(){
+    let curr = display.textContent
+    switch(currOp){
+        case '+':
+                display.textContent = parseFloat(curr) + parseFloat(pastNum);
+                break;
+        case '-':
+                display.textContent = +(pastNum) - parseFloat(curr);
+            break;
+        case 'X':
+                display.textContent = parseFloat(curr) * parseFloat(pastNum);
+            break;
+        case '/':
+                display.textContent = parseFloat(pastNum)/parseFloat(curr);
+            break;
+        default:
+    }
+    currOp = null;
+    pastNum = display.textContent;
+
+}
+
+function opVal(op){
+    if(opSimbol.includes(op)){
+        currOp = op;
+        pastNum = display.textContent;
+    }
+    else{
+        if(op === 'C'){
+            pastNum = null;
+            currOp = null;
+        }
+        display.textContent = '0';
+    }
+}
+
+function displayVal(number){
+    if(number !== '.'){
+        if(display.textContent === '0')
+            display.textContent = number;
+        else
+            display.textContent = display.textContent + number;
+    }
+    else{
+        if(display.textContent.includes('.'))
+            return
+        else
+            display.textContent = display.textContent + number;
+    }
+}
+
+keyNum.forEach(button=>{
+    button.addEventListener('click',e =>{
+        if(lastKey === '='){
+            opVal('C');
+        }
+        else if(opSimbol.includes(lastKey)){
+            display.textContent = '0';
+        }
+        displayVal(e.target.textContent);
+        lastKey = e.target.textContent;
+    })
+})
+
+
+keyOp.forEach(button=>{
+    button.addEventListener('click',e=>{
+        if(currOp === null){
+            opVal(e.target.textContent);
+            display.textContent = '0';
+        }
+        else{
+            calculate(currOp);
+            opVal(e.target.textContent);
+        }
+        lastKey = e.target.textContent;
+    })
+})
+
+keyEq.addEventListener('click',e=>{
+    calculate();
+    lastKey = e.target.textContent;
+})
